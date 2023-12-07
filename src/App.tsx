b71613c342d8742b9c3beae998a19e2aa8a799e1
MaetDol <rndka9@gmail.com>
2023-12-08 00:30:59 +0900

    비네팅 효과 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Film } from './components';
 import logo from './logo.svg';
@@ -6,6 +7,7 @@ import GITHUB_ICON from './statics/github.svg';
 import LEATHER_TEXTURE from './statics/leather_texture.jpg';
 
 const Background = styled.div`
+  pointer-events: none;
   background-color: #ffc225;
   position: absolute;
   width: 100%;
@@ -44,11 +46,56 @@ const GithubLink = styled.a`
   color: #222;
 `;
 
+const Vignetting = styled.div`
+  pointer-events: none;
+  font-size: 0;
+  width: 0;
+  position: absolute;
+
+  &::after {
+    content: '';
+    padding: 100%;
+    border-radius: 999px;
+    box-shadow: 0 0 320px 40px rgba(0, 0, 0, 0.2) inset;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
 function App() {
+  const [windowDiagonal, setWindowDiagonal] = useState(0);
+  const [windowCenter, setWindowCenter] = useState([0, 0]);
+
+  useEffect(() => {
+    const handler = () => {
+      // TODO: 디바운스 처리
+      const radius =
+        Math.sqrt(window.innerWidth ** 2 + window.innerHeight ** 2) / 2;
+      setWindowDiagonal(radius);
+      setWindowCenter([
+        window.innerWidth / 2 - radius,
+        window.innerHeight / 2 - radius,
+      ]);
+    };
+
+    handler();
+    window.addEventListener('resize', handler);
+    return () => window.removeEventListener('resize', handler);
+  }, []);
+
   return (
     <div className="App">
       <Background />
       <header className="App-header">
+        <Vignetting
+          style={{
+            width: `${windowDiagonal}px`,
+            left: `${windowCenter[0]}px`,
+            top: `${windowCenter[1]}px`,
+          }}
+        />
         <Film />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
