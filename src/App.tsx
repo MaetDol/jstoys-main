be76dfa2db02470c988029845257e20d89d9e74b
MaetDol <rndka9@gmail.com>
2023-12-07 20:50:33 +0900

    배경 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import styled from '@emotion/styled';
 import './App.css';
 import { Film } from './components';
 import logo from './logo.svg';
+import LEATHER_TEXTURE from './statics/leather_texture.jpg';
+
+const Background = styled.div`
+  background-color: #ffc225;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  &::before {
+    content: '';
+    background-image: url(${LEATHER_TEXTURE});
+    background-size: 400px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    opacity: 0.2;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <header className="App-header">
         <Film />
         <img src={logo} className="App-logo" alt="logo" />
