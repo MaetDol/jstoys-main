180fe6c199cc65829a5b71a2f4a666de4bdaa28a
MaetDol <rndka9@gmail.com>
2023-12-20 02:14:10 +0900

    #13 프로젝트 설명 보충
    주사위, 파이차트, k-means 설명 추가! 일단 이정도만 해놓으면 되겠죠? close #13

--- src/components/FilmPhysics/content.ts
+++ src/components/FilmPhysics/content.ts
@@ -44,21 +44,32 @@ export const FILM_CONTENTS: FilmContent[] = [
     demoUrl: 'https://maetdol.github.io/JSToys/3d-dice/',
     imgUrl: CSS_DICE,
     title: 'Css Dice',
-    description: `CSS 로 그려내고, JS 로 인터렉션 가능하게 만든 주사위에요`,
+    description: `CSS 로 그려내고, JS 로 인터렉션 가능하게 만든 주사위에요.
+    
+    단순 2D 로 렌더링된 주사위에 상호작용하는게 아닌, 
+    CSS 로 3D 로 만든 주사위를 직접 돌려보고, 
+    무작위 숫자가 나오게끔 굴리는 등의 조작이 가능해요`,
   },
 
   {
     demoUrl: 'https://maetdol.github.io/JSToys/pie-chart/',
     imgUrl: PIE_CHART,
     title: 'Pie chart',
-    description: `캔버스를 이용해 데이터를 파이 차트로 렌더링 하는 프로젝트.
-    특정 영역에 마우스를 호버하면, 비율을 툴팁으로 보여줘요.`,
+    description: `캔버스를 이용해 데이터를 파이 차트로 렌더링 하는 프로젝트!
+    특정 영역에 마우스를 호버하면, 비율을 툴팁으로 보여줘요.
+    
+    캔버스를 이용해 여러 형태의 파이차트를 렌더링하고 마우스 충돌을 직접 계산합니다.
+    툴팁 UI 캔버스 위로 나가지 않게 조절하는 등, 단순하지만 재밌었던 작업물이네요.`,
   },
   {
     demoUrl: 'https://maetdol.github.io/k-means-js/demo/',
     imgUrl: K_MEANS,
     title: 'k-means',
     description: `군집화(클러스터링) 알고리즘 중 하나인 k-means 를 직접 구현하고, 
-    어떤 방식으로 동작하는지 시각화 해놓은 프로젝트에요.`,
+    어떤 결과가 나오는지 차트로 표현한 프로젝트에요.
+    
+    인터렉티브 요소는 없지만, 많은 데이터들을 특정 목적을 위해 분석하고 가공하는 알고리즘을 직접 만든다는 점이 흥미로웠어요.
+    A 행동(값)이 있다면 B가 된다 같은 멱등성을 보장해야하고 그렇게 된다고 생각했는데,
+    알고리즘 특성상 되려 무작위성을 활용해야 한다는 점이 재밌었어요.`,
   },
 ].reverse();
