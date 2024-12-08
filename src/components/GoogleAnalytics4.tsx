9f6eebabba80e5fcedd5b867f050751ca3986aec
MaetDol <rndka9@gmail.com>
2024-12-08 22:46:31 +0900

    bugfix, GA4 동작 안하던 이슈 수정
    script 태그를 직접 사용하니 GA 가 동작하지 않아, useEffect 로 실행하게끔
    수정했어요.
    
    빠르게 마운트됐다 언마운트돼서 (strict mode) 요청이 취소된게 아닐까
    싶기도 한데, 정확하진 않군용.

--- src/components/GoogleAnalytics4.tsx
+++ src/components/GoogleAnalytics4.tsx
@@ -1,22 +1,39 @@
-export const GoogleAnalytics4 = () => {
-  return (
-    <>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-DT9Y6614LF"
-      ></script>
+import React, { useEffect } from 'react';
+import { processEnvService } from '../services/ProcessEnv';
 
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+export const GoogleAnalytics4 = React.memo(() => {
+  const isDebug = !processEnvService.isProducton();
+  if (isDebug) {
+    console.log('GA4 Running on DEBUG mode.');
+  }
 
-  gtag('config', 'G-DT9Y6614LF');
-  `,
-        }}
-      />
-    </>
-  );
-};
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-DT9Y6614LF';
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-DT9Y6614LF', { 'debug_mode': ${isDebug} });   
+    `;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+});
